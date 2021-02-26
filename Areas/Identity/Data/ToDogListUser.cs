84b3c01066f56ea81d71f4ac3d765533285fa8d6
Gil <marsh.skc@gmail.com>
2021-02-26 14:23:43 -0600

    added app controller and user authorization

--- Areas/Identity/Data/ToDogListUser.cs
+++ Areas/Identity/Data/ToDogListUser.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -10,6 +11,7 @@ namespace ToDogList.Areas.Identity.Data
     public class ToDogListUser : IdentityUser
     {
         [PersonalData]
+        [Required]
         public string Name { get; set; }
     }
 }
---
+++ Controllers/AppController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ToDogList.Areas.Identity.Data;
+
+namespace ToDogList.Controllers
+{
+    [Authorize]
+    public class AppController : Controller
+    {
+        private UserManager<ToDogListUser> userManager;
+
+        public AppController(UserManager<ToDogListUser> usrMgr)
+        {
+            userManager = usrMgr;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
